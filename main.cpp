7ce721d451164e3fe5d94fea9999aea01bfab4f8
Shiguang2737887 <123396527+Shiguang2737887@users.noreply.github.com>
2023-04-10 21:02:57 +0100

    Add files via upload
    Test function 4/10

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <fstream>
+#include <unordered_set>
+#include <zbar.h>
+#include <opencv2/opencv.hpp>
+
 using namespace std;
+using namespace cv;
+using namespace zbar;
 
 class StateMachine {
 public:
@@ -35,6 +43,81 @@ private:
     void decideState();//决定状态（目的地是哪：car1， car2， car3， source）
 };
 
+int move(){
+    Sleep(1000);
+    cout << "1 STEP" << endl;
+    return 0;
+}
+
+int turn(){
+    cout << "Turning" << endl;
+    Sleep(1000);
+    return 0;
+}
+
+string DetectQR()
+{
+    // Open the camera
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cerr << "Failed to open the camera." << endl;
+        return "";
+    }
+
+    namedWindow("QRCode Detector", cv::WINDOW_NORMAL);
+    resizeWindow("QRCode Detector", 640, 480);
+
+    // Create a zbar scanner
+    ImageScanner scanner;
+    scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+
+    // Create an unordered set to store scanned QR codes
+    unordered_set<string> scanned_qrcodes;
+
+    while (true) {
+        // Capture a new frame from the camera
+        Mat frame;
+        cap >> frame;
+
+        // Convert the frame to grayscale
+        Mat gray;
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+
+        // Wrap the grayscale image data in a zbar image
+        Image image(gray.cols, gray.rows, "Y800", (uchar*)gray.data, gray.cols * gray.rows);
+
+        // Scan the zbar image for QR codes
+        int n = scanner.scan(image);
+
+        // Loop over all the detected QR codes
+        for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
+            // Check if the QR code has already been scanned
+            if (scanned_qrcodes.find(symbol->get_data()) == scanned_qrcodes.end()) {
+                // Print the QR code content
+                cout << "Scanned QR code: " << symbol->get_data() << endl;
+
+                // Save the QR code content to a file
+                ofstream outfile("qrcode.txt", ios::app);
+                outfile << symbol->get_data() << endl;
+                outfile.close();
+
+                // Add the scanned QR code to the set
+                scanned_qrcodes.insert(symbol->get_data());
+                return symbol->get_data();//Return the scanned QR code content
+            }
+        }
+
+        imshow("QRCode Detector", frame);
+
+        // Clean up
+        image.set_data(nullptr, 0);
+        if (waitKey(30) >= 0) {
+            break;
+        }
+    }
+    return "";//Return empty string if no QR code is scanned
+}
+
 void StateMachine::run() {
     while (true) {
         switch (currentState) {
@@ -57,20 +140,40 @@ void StateMachine::run() {
 void StateMachine::scanState() {
     cout << "State: Scanning" << endl;
     a = 0;
-    //DetectQR(); //调用检测二维码的函数
+    DetectQR(); //调用检测二维码的函数
     currentState = DECIDE;
 }
 
 void StateMachine::dischargeState() {
     cout << "State: Unloading" << endl;
     a = 1;
+    current_pos[0] = goal_pos[0];
+    current_pos[1] = goal_pos[1];
     currentState = NAVIGATION; //新的状态
 }
 
 void StateMachine::navigationState() {
+    double start, stop, duration;
+    int x,y;
+    x = goal_pos[0];
+    y = goal_pos[1];
     cout << "State: Navigation" << endl;
     cout << a << endl;
+    cout << x << endl;
+    cout << y << endl;
+    start = clock();
     if (a == 0){
+        for(int i = 0;i < y; i++){
+            move();
+        }
+        turn();
+        for(int i = 0;i < x; i++){
+            move();
+        }
+        turn();
+        stop = clock();
+        duration = ((double)(stop - start))/CLK_TCK;
+        cout << "Time cost:"<< duration << endl << endl;
         currentState = DISCHARGE;}
     else{
         currentState = SCAN;}
@@ -138,3 +241,4 @@ int main() {
 
     return 0;
 }
+
