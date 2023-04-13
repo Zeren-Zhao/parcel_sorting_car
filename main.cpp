a46e720b4664a8bc5bc5a6bf1ade721f5bd8d100
ZerenZhao <123029826+ZerenZhao@users.noreply.github.com>
2023-04-13 22:57:26 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <thread>
-#include <string>
 #include <mutex>
-
-#include "state_machine.h"
-#include "QRCodeScanner.h"
-
+#include "state_machine.cpp"
+#include "QR.cpp"
+#include <string>
 int main() {
 
-    int car_pos1[2] = {1, 2};
-    int car_pos2[2] = {3, 4};
-    int car_pos3[2] = {5, 6};
-    int source_pos[2] = {7, 8};
+    int car_pos1[2] = {3, 4};
+    int car_pos2[2] = {-3, 3};
+    int car_pos3[2] = {2, 2};
+    int source_pos[2] = {0, 0};
 
     //初始化变量
-    std::string location = "";
-    int current_1 = 1;
-    int current_2 = 1;
-    std::mutex mtx;
+    string location = "";
+    int current_1 = 0;
+    int current_2 = 0;
+    mutex mtx;
+
+    /*class A {
+    public:
+        std::string scanQRCode() {
+            // 扫描二维码并提取信息
+            std::string info = "example QR code info";
+            return info;
+        }
+    };*/
 
     QRCodeScanner detector(0);
     StateMachine machine(car_pos1, car_pos2, car_pos3, source_pos, current_1, current_2, location);
@@ -26,13 +33,13 @@ int main() {
         while(true){
 
             std::string info = detector.DetectQR();
+
             // 当car[2] == {1, 1}并且字符串loc == “”时赋给字符串loc
-            if (current_1 == 1 && current_2 == 1 && location.empty()) {
+            if (current_1 == 0 && current_2 == 0 && location.empty()) {
                 std::lock_guard<std::mutex> lock(mtx);
                 location = info;
-                cout << location << endl;
-
-            }}
+                cout << "cao" << endl;
+        }}
     });
 
     std::thread threadB([&]() {
--- state_machine.cpp
+++ state_machine.cpp
@@ -1,18 +1,60 @@
-#include "state_machine.h"
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+#include "control.cpp"
 
-StateMachine::StateMachine(int car_pos1[2], int car_pos2[2], int car_pos3[2], int source_pos[2], int& current_1, int& current_2, string& location):current_1(current_1),current_2(current_2),location(location){
-    for (int i = 0; i < 2; i++) {
-        car1[i] = car_pos1[i];
-        car2[i] = car_pos2[i];
-        car3[i] = car_pos3[i];
-        source[i] = source_pos[i];
-        currentState = SCAN;
-    }
+using namespace std;
+
+void wait_time(int ms) {
+    std::thread t([ms]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    });
+    t.join();
 }
 
 
+class StateMachine {
+public:
+
+    void run(); // 运行状态机
+
+    //从外部更新private中的数据
+    StateMachine(int car_pos1[2], int car_pos2[2], int car_pos3[2], int source_pos[2], int& current_1, int& current_2, string& location):current_1(current_1),current_2(current_2),location(location){
+        for (int i = 0; i < 2; i++) {
+            car1[i] = car_pos1[i];
+            car2[i] = car_pos2[i];
+            car3[i] = car_pos3[i];
+            source[i] = source_pos[i];
+            currentState = SCAN;
+        }
+    };
+
+private:
+
+    bool stopFlag = false;
+
+    int& current_1;
+    int& current_2;
+    string& location;
+
+    enum State { SCAN, NAVIGATION, DISCHARGE, DECIDE }; // 状态类型
+    State currentState; // 当前状态
+
+
+    int a = 0; // 参数a
+    int current_pos[2], goal_pos[2];//小车的现在位置以及目标位置
+    int car1[2], car2[2], car3[2];//三个目的地的位置
+    int source[2];//取货处
+
+    void scanState(); // 扫描二维码状态
+    void navigationState(); // 路径规划状态
+    void dischargeState(); // 卸货状态
+    void decideState();//决定状态（目的地是哪：car1， car2， car3， source）
+};
+
 void StateMachine::run() {
-    // run函数实现
     stopFlag = false;
     while (!stopFlag) {
         switch (currentState) {
@@ -32,51 +74,126 @@ void StateMachine::run() {
     }
 }
 
-void StateMachine::navigationState() {
-    // navigationState函数实现
-    cout << "navigation" << endl;
-    if (a == 0){
-        currentState = DISCHARGE;}
-    else{
-        current_1 = 1;
-        current_2 = 1;
-        location = "";
-        stopFlag = true;
-        currentState = SCAN;
-    }
-}
-
 void StateMachine::scanState() {
-    // scanState函数实现
     cout << "scan" << endl;
     a = 0;
     currentState = DECIDE;
 }
 
 void StateMachine::dischargeState() {
-    // dischargeState函数实现
     cout << "discharge" << endl;
     a = 1;
-    currentState = NAVIGATION; //新的状态
+    currentState = DECIDE; //新的状态
+}
+
+void StateMachine::navigationState() {
+    cout << "navigation" << endl;
+    cout << goal_pos[0] << endl;
+    cout << goal_pos[1] << endl;
+    wiringPiSetup();
+    L298N driver(26, 29, 27, 28);
+    int x,y,turn;
+    x = abs(goal_pos[0]-current_1)*500;
+    y = abs(goal_pos[1]-current_2)*500;
+    turn = 480;
+    cout << x << endl;
+    cout << y << endl;
+    
+
+    if(current_2 >= goal_pos[1]) {
+        current_2 = goal_pos[1];
+        driver.retreat();
+        wait_time(y);
+
+        driver.stop();
+    }
+    else{
+        current_2 = goal_pos[1];
+        driver.forward();
+        wait_time(y);
+
+        driver.stop();
+    }
+
+    if(current_1 >= goal_pos[0]) {
+        current_1 = goal_pos[0];
+        driver.left();
+        wait_time(turn);
+
+        driver.stop();
+        driver.forward();
+        wait_time(x);
+
+        driver.stop();
+        driver.right();
+        wait_time(turn);
+
+        driver.stop();
+    }
+    else {
+        current_1 = goal_pos[0];
+        driver.right();
+
+        wait_time(turn);
+
+        driver.stop();
+        driver.forward();
+
+        wait_time(x);
+
+        driver.stop();
+        driver.left();
+
+        wait_time(turn);
+
+        driver.stop();
+    }
+
+    if (a == 0){
+        currentState = DISCHARGE;}
+    else{
+        location = "";
+        stopFlag = true;
+        currentState = SCAN;}
 }
 
 void StateMachine::decideState() {
-    // decideState函数实现
     cout << "decision" << endl;
-    current_1 = 2;
-    current_2 = 2;
+    string information = location.substr(0, location.find(" "));
+
     if (a==1){
         for (int i = 0; i < 2; i++) {
             goal_pos[i] = source[i];
         }
+        currentState = NAVIGATION;
     }
     else{
-        for (int i = 0; i < 2; i++) {
-            goal_pos[i] = car1[i];
+
+        if (information.compare("G1") == 0|information.compare("G2") == 0|information.compare("G3") == 0){
+            cout << "Place A" << endl;
+            for (int i = 0; i < 2; i++) {
+                goal_pos[i] = car1[i];
+            }
+            currentState = NAVIGATION;
         }
-    }
-    currentState = NAVIGATION;
-    if (currentState == DISCHARGE) {
-        stopFlag = true;
+        else if (information.compare("G4") == 0|information.compare("G5") == 0|information.compare("G6") == 0){
+            cout << "Place B" << endl;
+            for (int i = 0; i < 2; i++) {
+                goal_pos[i] = car2[i];
+            }
+
+            currentState = NAVIGATION;
+        }
+        else if (information.compare("G12") == 0|information.compare("G13") == 0|information.compare("G67") == 0){
+            cout << "Place C"<< endl;
+            for (int i = 0; i < 2; i++) {
+                goal_pos[i] = car3[i];
+            }
+            currentState = NAVIGATION;
+        }
+        else{
+            cout << "Wrong zip code" << endl;
+            currentState = SCAN;
+        }
     }
 }
