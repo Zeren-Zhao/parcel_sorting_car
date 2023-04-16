4ab02af4a42f6218e86fe21aa973536a229fcb80
ZerenZhao <123029826+ZerenZhao@users.noreply.github.com>
2023-04-16 20:07:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 #include "state_machine.cpp"
 #include "QR.cpp"
 #include <string>
+#include "camera.cpp"
+
 int main() {
 
+    int camera_index = 0;
+    
     int car_pos1[2] = {3, 4};
     int car_pos2[2] = {-3, 3};
     int car_pos3[2] = {2, 2};
@@ -16,8 +20,9 @@ int main() {
     int current_1 = 0;
     int current_2 = 0;
     mutex mtx;
+    
+    QRCodeScanner scanner;
 
-    QRCodeScanner detector(0);
     StateMachine machine(car_pos1, car_pos2, car_pos3, source_pos, current_1, current_2, location);
 
     auto callback = [&](const std::string& info) {
@@ -28,11 +33,13 @@ int main() {
     };
 
 
-    while (true) {
-        detector.DetectQR(callback);
-    }
+    thread capture_thread(CaptureFrames, camera_index);
+    thread detection_thread(&QRCodeScanner::DetectQR, &scanner, callback);
 
+    capture_thread.join();
+    detection_thread.join();
 
 
     return 0;
 }
+
