7a87a168c42b78ad55d4a8b942de840b64462d09
ZerenZhao <123029826+ZerenZhao@users.noreply.github.com>
2023-04-16 20:03:57 +0100

    Update QR.cpp

--- QR.cpp
+++ QR.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <fstream>
-#include <unordered_set>
+#include <queue>
+#include <functional>
 #include <zbar.h>
 #include <opencv2/opencv.hpp>
-
-
 
 using namespace std;
 using namespace cv;
@@ -14,63 +12,45 @@ class QRCodeScanner {
 public:
     using Callback = std::function<void(const std::string&)>;
 
-    QRCodeScanner(int camera_index = 0) : camera_index_(camera_index) {}
+    QRCodeScanner() {}
 
     void DetectQR(const Callback& callback) {
-        VideoCapture cap(camera_index_);
-        if (!cap.isOpened()) {
-            cerr << "Failed to open the camera." << endl;
-            return;
-        }
-
-        namedWindow("QRCode Detector", cv::WINDOW_NORMAL);
-        resizeWindow("QRCode Detector", 640, 480);
-
         ImageScanner scanner;
         scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
 
         unordered_set<string> scanned_qrcodes;
 
         while (true) {
-            Mat frame;
-            cap >> frame;
+            if (!frame_queue.empty()) {
+                Mat frame = frame_queue.front();
+                frame_queue.pop();
 
-            Mat gray;
-            cvtColor(frame, gray, COLOR_BGR2GRAY);
+                Mat gray;
+                cvtColor(frame, gray, COLOR_BGR2GRAY);
 
-            Image image(gray.cols, gray.rows, "Y800", (uchar*)gray.data, gray.cols * gray.rows);
+                Image image(gray.cols, gray.rows, "Y800", (uchar*)gray.data, gray.cols * gray.rows);
 
-            int n = scanner.scan(image);
+                int n = scanner.scan(image);
 
-            for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
-                if (scanned_qrcodes.find(symbol->get_data()) == scanned_qrcodes.end()) {
-                    cout << "Scanned QR code: " << symbol->get_data() << endl;
+                for (Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) {
+                    if (scanned_qrcodes.find(symbol->get_data()) == scanned_qrcodes.end()) {
+                        cout << "Scanned QR code: " << symbol->get_data() << endl;
 
-                    ofstream outfile("qrcode.txt", ios::app);
-                    outfile << symbol->get_data() << endl;
-                    outfile.close();
+                        ofstream outfile("qrcode.txt", ios::app);
+                        outfile << symbol->get_data() << endl;
+                        outfile.close();
 
-                    scanned_qrcodes.insert(symbol->get_data());
+                        scanned_qrcodes.insert(symbol->get_data());
 
-                    // Calling the callback function
-                    callback(symbol->get_data());
-                    
+                        // Calling the callback function
+                        callback(symbol->get_data());
+                    }
                 }
-                
-            }
-            
 
-            imshow("QRCode Detector", frame);
-
-            image.set_data(nullptr, 0);
-            if (waitKey(30) >= 0) {
-                break;
+                image.set_data(nullptr, 0);
             }
-            break;
-            
         }
     }
-
-private:
-    int camera_index_;
 };
+
+
